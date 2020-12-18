ace44de22d8086dea6687b977ec18852b4c4c7fe
= <=>
2020-12-18 21:04:06 +0700

    Fix bug

--- AddiMatrix.v
+++ AddiMatrix.v
@@ -1,28 +1,29 @@
-module AddiMatrix(I, Out);
+module AddiMatrix(I0, I1, I2, I3, I4, I5, I6, I7, I8, I9, I10, I11, I12, I13, I14, I15, 
+				O0, O1, O2, O3, O4, O5, O6, O7, O8, O9, O10, O11, O12, O13, O14, O15);
 
-	input [31:0] I [15:0];
-	output [31:0] Out [15:0];
+	input [31:0] I0, I1, I2, I3, I4, I5, I6, I7, I8, I9, I10, I11, I12, I13, I14, I15;
+	output [31:0] O0, O1, O2, O3, O4, O5, O6, O7, O8, O9, O10, O11, O12, O13, O14, O15;
 	
 	reg [31:0] In [15:0];
 	reg [31:0] Temp1 [15:0];
 	reg [31:0] Temp2 [15:0];
 	
-	Sum s00(I[15], 32'b0, In[0]);
-	Sum s01(I[14], 32'b0, In[1]);
-	Sum s02(I[13], 32'b0, In[2]);
-	Sum s03(I[12], 32'b0, In[3]);
-	Sum s04(I[11], 32'b0, In[4]);
-	Sum s05(I[10], 32'b0, In[5]);
-	Sum s06(I[9], 32'b0, In[6]);
-	Sum s07(I[8], 32'b0, In[7]);
-	Sum s08(I[7], 32'b0, In[8]);
-	Sum s09(I[6], 32'b0, In[9]);
-	Sum s10(I[5], 32'b0, In[10]);
-	Sum s11(I[4], 32'b0, In[11]);
-	Sum s12(I[3], 32'b0, In[12]);
-	Sum s13(I[2], 32'b0, In[13]);
-	Sum s14(I[1], 32'b0, In[14]);
-	Sum s15(I[0], 32'b0, In[15]);
+	Sum s00(I15, 32'b0, In[0]);
+	Sum s01(I14, 32'b0, In[1]);
+	Sum s02(I13, 32'b0, In[2]);
+	Sum s03(I12, 32'b0, In[3]);
+	Sum s04(I11, 32'b0, In[4]);
+	Sum s05(I10, 32'b0, In[5]);
+	Sum s06(I9, 32'b0, In[6]);
+	Sum s07(I8, 32'b0, In[7]);
+	Sum s08(I7, 32'b0, In[8]);
+	Sum s09(I6, 32'b0, In[9]);
+	Sum s10(I5, 32'b0, In[10]);
+	Sum s11(I4, 32'b0, In[11]);
+	Sum s12(I3, 32'b0, In[12]);
+	Sum s13(I2, 32'b0, In[13]);
+	Sum s14(I1, 32'b0, In[14]);
+	Sum s15(I0, 32'b0, In[15]);
 	
 	Det3x3 d00(In[5], In[6], In[7], In[9], In[10], In[11], In[13], In[14], In[15], Temp1[0]);
 	Det3x3 d01(In[4], In[6], In[7], In[8], In[10], In[11], In[12], In[14], In[15], Temp1[1]);
@@ -61,6 +62,21 @@ module AddiMatrix(I, Out);
 	Sub t14(32'b0, Temp1[14], Temp2[14]);
 	Sum t15(32'b0, Temp1[15], Temp2[15]);
 	
-	assign Out = Temp2;
+	assign O0 = Temp2[0];
+	assign O1 = Temp2[1];
+	assign O2 = Temp2[2];
+	assign O3 = Temp2[3];
+	assign O4 = Temp2[4];
+	assign O5 = Temp2[5];
+	assign O6 = Temp2[6];
+	assign O7 = Temp2[7];
+	assign O8 = Temp2[8];
+	assign O9 = Temp2[9];
+	assign O10 = Temp2[10];
+	assign O11 = Temp2[11];
+	assign O12 = Temp2[12];
+	assign O13 = Temp2[13];
+	assign O14 = Temp2[14];
+	assign O15 = Temp2[15];
 	
 endmodule
--- InvMatrix.v
+++ InvMatrix.v
@@ -1,14 +1,17 @@
-module InvMatrix(In, Out);
+module InvMatrix(I0, I1, I2, I3, I4, I5, I6, I7, I8, I9, I10, I11, I12, I13, I14, I15,
+				O0, O1, O2, O3, O4, O5, O6, O7, O8, O9, O10, O11, O12, O13, O14, O15);
 
-	input [31:0] In [15:0];
-	output [31:0] Out [15:0];
+	input [31:0] I0, I1, I2, I3, I4, I5, I6, I7, I8, I9, I10, I11, I12, I13, I14, I15;
+	output [31:0] O0, O1, O2, O3, O4, O5, O6, O7, O8, O9, O10, O11, O12, O13, O14, O15;
 	
 	reg [31:0] Temp1 [15:0];
 	reg [31:0] Temp2 [15:0];
 	reg [31:0] Temp3;
 
-	AddiMatrix a1(In, Temp1);
-	Det4x4 b1(In, Temp3);
+	AddiMatrix a1(I0, I1, I2, I3, I4, I5, I6, I7, I8, I9, I10, I11, I12, I13, I14, I15, 
+	Temp1[0], Temp1[1], Temp1[2], Temp1[3], Temp1[4], Temp1[5], Temp1[6], Temp1[7], Temp1[8], Temp1[9], Temp1[10], Temp1[11], Temp1[12], Temp1[13], Temp1[14], Temp1[15]);
+	
+	Det4x4 b1(I0, I1, I2, I3, I4, I5, I6, I7, I8, I9, I10, I11, I12, I13, I14, I15, Temp3);
 	
 	Div d00(Temp1[0], Temp3, Temp2[0]);
 	Div d01(Temp1[1], Temp3, Temp2[1]);
@@ -27,6 +30,21 @@ module InvMatrix(In, Out);
 	Div d14(Temp1[14], Temp3, Temp2[14]);
 	Div d15(Temp1[15], Temp3, Temp2[15]);
 	
-	assign Out = Temp2;
+	assign O0 = Temp2[0];
+	assign O1 = Temp2[1];
+	assign O2 = Temp2[2];
+	assign O3 = Temp2[3];
+	assign O4 = Temp2[4];
+	assign O5 = Temp2[5];
+	assign O6 = Temp2[6];
+	assign O7 = Temp2[7];
+	assign O8 = Temp2[8];
+	assign O9 = Temp2[9];
+	assign O10 = Temp2[10];
+	assign O11 = Temp2[11];
+	assign O12 = Temp2[12];
+	assign O13 = Temp2[13];
+	assign O14 = Temp2[14];
+	assign O15 = Temp2[15];
 	
 endmodule
